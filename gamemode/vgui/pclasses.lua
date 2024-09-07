505e47dd60f80dcd4e7339ca314c110e62bebe97
TonyDoskEnginooy <111401791+TonyDoskEnginooy@users.noreply.github.com>
2024-09-06 17:54:36 -0700

    Add files via upload

--- gamemode/vgui/pclasses.lua
+++ gamemode/vgui/pclasses.lua
@@ -9,29 +9,35 @@ function MakepClasses()
 		pClasses:Remove()
 		pClasses = nil
 	end
+	local h = ScrH()
+	local w = ScrW()
 
 	local Window = vgui.Create("DFrame")
-	local wide = 500 -- w * 0.5
-	local tall = 650 -- h * 0.95
+	local wide = w * 0.25
+	local tall = h * 0.95
 
 	Window:SetSize(wide, tall)
 	Window:CenterVertical()
 	Window:CenterHorizontal()
-	--Window:SetPos(w * 0.25, h * 0.025)
+	Window:SetPos(w * 0.375, h * 0.025)
 	Window:SetTitle(" ")
 	Window:SetVisible(true)
 	Window:SetDraggable(false)
 	Window:MakePopup()
 	Window:SetDeleteOnClose(false)
+	Window:SetScreenLock(true)
 	Window:SetCursor("pointer")
 	pClasses = Window
 
+	local DScrollPanel = vgui.Create( "DScrollPanel", Window )
+	DScrollPanel:Dock( FILL )
+
 	surface.SetFont("HUDFontAA")
 	local tw, th = surface.GetTextSize("Choose a class...")
-	local label = vgui.Create("DLabel", Window)
+	local label = vgui.Create("DLabel", DScrollPanel)
 	label:SetY(25)
 	label:SetSize(tw, th)
-	label:CenterHorizontal()
+	label:SetPos(w * 0.08, h * 0.01)
 
 	label:SetFont("HUDFontAA")
 	label:SetText("Choose a class...")
@@ -39,19 +45,22 @@ function MakepClasses()
 
 	local y = 95
 
+	print(w)
+	print(h)
+
 	for i, class in ipairs(ZombieClasses) do
 		if not class.Hidden then
-			local button = vgui.Create("SpawnIcon", Window)
+			local button = vgui.Create("SpawnIcon", DScrollPanel)
 			button:SetPos(41, y)
-			button:SetSize(48, 48)
+			button:SetSize(w * 0.04, h * 0.07)
 			button:SetModel(class.Model)
 			button.Class = class
 			button.OnMousePressed = SwitchClass
 
 			surface.SetFont("HUDFontSmallAA")
 			local tw, th = surface.GetTextSize(class.Name)
-			local label = vgui.Create("DLabel", Window)
-			label:SetPos(button:GetWide() + 49, y + 2)
+			local label = vgui.Create("DLabel", DScrollPanel)
+			label:SetPos(button:GetWide() + 49, y - 30)
 			label:SetSize(tw, th)
 			label:SetFont("HUDFontSmallAA")
 			label:SetText(class.Name)
@@ -65,8 +74,8 @@ function MakepClasses()
 			for i, line in ipairs(string.Explode("@", class.Description)) do
 				surface.SetFont("Default")
 				local tw, th = surface.GetTextSize(line)
-				local label = vgui.Create("DLabel", Window)
-				label:SetPos(button:GetWide() + 52, yy)
+				local label = vgui.Create("DLabel", DScrollPanel)
+				label:SetPos(button:GetWide() + 52, yy - 30)
 				label:SetSize(tw, th)
 				label:SetFont("Default")
 				label:SetText(line)
