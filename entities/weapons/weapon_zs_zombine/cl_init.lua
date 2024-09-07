8ed3d226082d402f0d43f0de69308712dd791c6c
TonyDoskEnginooy <111401791+TonyDoskEnginooy@users.noreply.github.com>
2024-09-07 16:14:47 -0700

    Add files via upload

--- entities/weapons/weapon_zs_zombine/cl_init.lua
+++ entities/weapons/weapon_zs_zombine/cl_init.lua
@@ -2,3 +2,9 @@ include("shared.lua")
 
 SWEP.PrintName = "Zombine"
 SWEP.ViewModelFOV = 50
+
+hook.Add("PlayerBindPress", "DisableJump", function(ply, bind, pressed)
+    if ply:GetZombieClass() == 10 and ply:GetMaxSpeed() == 1 and bind == "+jump" then
+        return true
+    end
+end)
